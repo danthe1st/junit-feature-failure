5b2a2f8f6148b4405353700f7ca4e6ac88ad9108
danthe1st <daniel@wwwmaster.at>
2023-11-25 23:18:45 +0100

    disabling tests is actually not necessary

--- src/test/java/io/github/danthe1st/junitfeaturefailure/SubTest.java
+++ src/test/java/io/github/danthe1st/junitfeaturefailure/SubTest.java
@@ -1,10 +1,7 @@
 package io.github.danthe1st.junitfeaturefailure;
 
-import org.junit.jupiter.api.Disabled;
-
 class SubTest extends SuperTest {
 	@Override
-	@Disabled
 	public void test() throws Exception {
 	}
 }
